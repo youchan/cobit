80c2e09fc5164e51a45e1ff910096dc56b18751d
youchan <youchan@diana.(none)>
2013-03-07 04:31:09 +0900

    ファイルリストの実装

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 # Specify your gem's dependencies in sinatra-cometio.gemspec
 gemspec
--- cobit.rb
+++ cobit.rb
@@ -20,12 +20,13 @@ class Cobit
       @@instance.session = session
     end
     
-    def render(path)
+    def open(path)
       @path = path
-      render
+      CometIO.push :cobit, { :content => render }
     end
 
     def render
+      @path = "data/README.md" if not File.exist? @path
       content = @redcarpet.render(File.read(@path))
       #puts content
       #CometIO.push :cobit, { :content => content }
@@ -41,13 +42,17 @@ class Cobit
         update do|base, file|
           puts "UPDATE: #{base} #{file}"
           #@@renderer.render
-          CometIO.push :cobit, { :content => @@renderer.render }
+          if (file == File.basename(@@renderer.path))
+            CometIO.push :cobit, { :content => @@renderer.render }
+          end
         end
         create do|base, file|
-          CometIO.push :cobit, { :content => @@renderer.render(file) }
+          puts "CREATE: #{base} #{file}"
+          CometIO.push :filelist, { :type => "add", :filename => file }
         end
         delete do|base, file|
           puts "DELETE: #{base} #{file}"
+          CometIO.push :filelist, { :type => "remove", :filename => file }
         end
       end
     end
@@ -62,7 +67,11 @@ class Cobit
   end
 
   def self.render
-            @@renderer.render
+    @@renderer.render
+  end
+
+  def self.open(path)
+    @@renderer.open path
   end
 end
 
@@ -73,3 +82,8 @@ get "/" do
   @filelist = Dir.entries("data").select {|f| File.extname(f) == ".md"}
   erb :index
 end 
+
+get "/open/:name" do
+  Cobit.open "data/" + params[:name]
+end
+
